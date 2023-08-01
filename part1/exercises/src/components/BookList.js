9f1b4c6646cfbff3546e5a235566a22c0001d86a
varti2023 <varti.pant@gmail.com>
2023-08-01 16:04:09 -0500

    chores and hobbies part1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,17 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Books Catalogue";
+   let book1 = "https://www.beyondthebookends.com/wp-content/uploads/2023/05/The-Six.jpg";
+   let book2 = "https://www.beyondthebookends.com/wp-content/uploads/2023/02/The-Little-Village-of-Book-Lovers-.jpg";
+   let book3 = "https://www.beyondthebookends.com/wp-content/uploads/2023/04/The-Invisible-Hour.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="The Invisible Hour" />
+         &nbsp; &nbsp;
+         <img src={book2} alt="The Little Village of Book Lovers" />
+         &nbsp;  &nbsp;   
+         <img src={book3} alt="The Six: The Untold Story of the First Women Astronauts" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,15 @@
+import styles from './ChoresList.module.css';
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   
+   const chore = ["Cleaning Rooms","Preparing Dinner","Gardening"];
+   return (
+      <div>
+       <p className = {styles.choresHeading}>Chores List</p>
+         <ol>
+            <li className= {styles.choresText}>{chore[0]}</li>
+            <li className= {styles.choresText}>{chore[1]}</li>
+            <li className= {styles.choresText}>{chore[2]}</li>
+         </ol>
+   </div>
+   );
 }
